c34bf4eed2ab757797aae044f1f4839e2915cfc6
Ahamed Sheak Ansari S <80703598+Ahamed13@users.noreply.github.com>
2022-01-16 08:32:26 +0530

    Add files via upload

---
+++ Addingofarray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AddofArray
+{
+    class Addingofarray
+    {
+        static int Addarray(int x, string y)
+        {
+            int z = 0;
+            string[] arr = y.Split(" ");
+            if(x == arr.Length)
+            {
+                foreach(string item in arr)
+                {
+                    z += Convert.ToInt32(item);
+
+                }
+            }
+            return z;
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number : ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Enter the Array : ");
+            string y = Console.ReadLine();
+            int result = Addarray(x, y);
+            Console.WriteLine(result);
+        }
+    }
+}
